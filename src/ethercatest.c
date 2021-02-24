58bb8240fc254a29c4f3887f58483ca0965f2b1a
Nicola Fontana <ntd@entidi.it>
2021-02-24 17:48:11 +0100

    Add notice to fulfill GPL2

--- src/ethercatest.c
+++ src/ethercatest.c
@@ -1,4 +1,22 @@
-/* Adapted from SOEM simple_test.c */
+/* Adapted from SOEM simple_test.c
+ *
+ * ethercatest: test EtherCAT connection by monitoring I/O.
+ * Copyright (C) 2021  Fontana Nicola <ntd at entidi.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #include <soem/ethercat.h>
 #include <glib.h>
